974e86b51d9f4f0775f4e75e7c037fa79ac2e6c4
Kauan Cerqueira <kauan.cerqueira0198@gmail.com>
2025-10-01 21:58:00 -0300

    subi aqui tbm

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
